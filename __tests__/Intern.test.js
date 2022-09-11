8ead3ccaeefcc98a597835fd871ec5c0aaf75e57
Jacob Lockhart <procoder165@gmail.com>
2022-09-11 18:40:43 -0400

    finished intern test cases

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,5 +1,26 @@
 const Intern = require("../lib/Intern");
-
+const test = new Intern("Jacob", 123123, "098@gmail.com", "UNCC")
 describe("Intern", () => {
+    describe("Init", () =>{
+        it(" should save the values called name, id ,email, and school", () =>{
+            expect("name" in test).toEqual(true);
+            expect("id" in test).toEqual(true);
+            expect("email" in test).toEqual(true);
+            expect("school" in test).toEqual(true);
+        })
+    })
+    describe("getRole()", () =>{
+        it("Should return Intern", () =>{
+            const result = test.getRole();
 
+            expect(result).toEqual("Intern");
+        })
+    })
+    describe("getSchool()", () =>{
+        it("Should return a value called school", () =>{
+            const result = test.getSchool();
+
+            expect(result).toEqual("UNCC");
+        })
+    })
 });
