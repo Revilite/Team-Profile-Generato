7b825e118a7c3562ec5006f1c5f50d49a5b1115c
Jacob Lockhart <procoder165@gmail.com>
2022-09-11 18:47:50 -0400

    finished with engineer test cases

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,5 +1,27 @@
 const Engineer = require("../lib/Engineer");
-
+const test = new Engineer("Jacob", 123123, "321@gmail.com", "Revilite")
 describe("Engineer", () => {
+    describe("Init", () =>{
+        it(" should save the values called name, id ,email, and github", () =>{
+            expect("name" in test).toEqual(true);
+            expect("id" in test).toEqual(true);
+            expect("email" in test).toEqual(true);
+            expect("github" in test).toEqual(true);
+        })
+})
+    describe("getRole", () =>{
+        it("should return Engineer", () =>{
+            const result = test.getRole();
 
+            expect(result).toEqual("Engineer");
+        })
+    })
+    describe("getGithub", () =>{
+        it("should return a value named github", () =>{
+            const result = test.getGithub();
+
+            expect(result).toEqual("Revilite");
+        })
+    })
 });
+
