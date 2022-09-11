d6a05bf9bd3fdd030b3ab123679e6808bbf0ccc5
Jacob Lockhart <procoder165@gmail.com>
2022-09-11 18:34:51 -0400

    finished manager test cases

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,6 +1,28 @@
 const Manager = require("../lib/Manager");
-
+const test = new Manager("Jacob", 123123, "123@gmail.com", 321)
 describe("Manager", () => {
+    describe("Init", () =>{
+        it(" should save the values called name, id ,email, and number", () =>{
+            expect("name" in test).toEqual(true);
+            expect("id" in test).toEqual(true);
+            expect("email" in test).toEqual(true);
+            expect("number" in test).toEqual(true);
+        })
+    })
+    describe("getRole()", () =>{
+        it("should return Manager", ()=>{
+            const result = test.getRole();
 
+            expect(result).toEqual("Manager")
+        });
+        
+    })
+    describe("getOfficeNumber", ()=>{
+        it("should return the value called number", () =>{
+            const result = test.getOfficeNumber();
+
+            expect(result).toEqual(321);
+        })
+    })
 });
 
